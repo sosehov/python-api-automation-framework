251c3ce8cade99942d43de929740c80a0291a109
Soseh <sosehv@gmail.com>
2026-04-30 16:46:16 -0400

    Improve test data structure, add README, enhance tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,9 +1,9 @@
 import pytest
 from utils.login_utils import login
-from data.test_data import users
+from data.test_data import login_test_data
 
 
-@pytest.mark.parametrize("user", users)
+@pytest.mark.parametrize("user", login_test_data)
 def test_login(user):
     result = login(user["username"], user["password"])
     assert result == user["expected"]
